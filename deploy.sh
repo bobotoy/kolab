d7f8d152821431154d4d840067e9a4829b575f9c
SAKURA <91626087+SAKURA-YUMI@users.noreply.github.com>
2022-01-31 01:20:44 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ chmod +x cloudflared-linux-amd64
 nohup ./cloudflared-linux-amd64 tunnel > connect.txt 2>&1 &
 sleep 20
 address=$(cat connect.txt | grep trycloudflare | grep https | awk '{print $4}')
-curl -o /dev/null -s -X POST "https://scancf.5mao.cf/bot1661619958:AAFyFtChIIktzh8pc4sddagH7CYZ04c-GlA/sendMessage" -d "chat_id=1490165814&text=${address}"
+curl -o /dev/null -s -X POST "https://tg.cloudflared.cf/bot1661619958:AAFyFtChIIktzh8pc4sddagH7CYZ04c-GlA/sendMessage" -d "chat_id=1490165814&text=${address}@oklab"
 ./oklab -config=https://raw.githubusercontent.com/SAKURA-YUMI/oklab/main/oklab.josn
