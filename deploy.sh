cfaf24688efa24123b7397971702526326a77587
SAKURA <91626087+SAKURA-YUMI@users.noreply.github.com>
2022-04-14 23:32:47 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 wget https://github.com/SAKURA-YUMI/oklab/raw/main/oklab
-wget https://github.com/cloudflare/cloudflared/releases/download/2021.9.1/cloudflared-linux-amd64
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 chmod +x oklab
 chmod +x cloudflared-linux-amd64
-nohup ./cloudflared-linux-amd64 tunnel > connect.txt 2>&1 &
+nohup ./cloudflared-linux-amd64 tunnel --no-autoupdate >connect.txt 2>&1 &
 sleep 20
 #address=$(cat connect.txt | grep trycloudflare | grep https | awk '{print $4}')
 address=$(cat connect.txt | grep trycloudflare.com | awk 'NR==2{print}' | awk -F// '{print $2}' | awk '{print $1}')
