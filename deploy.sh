475d4e45cf8648f29aa934fa5bb4c24998e54400
SAKURA <91626087+SAKURA-YUMI@users.noreply.github.com>
2023-06-14 00:01:44 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ nohup ./cloudflared-linux-amd64 tunnel --no-autoupdate >connect.txt 2>&1 &
 sleep 20
 #address=$(cat connect.txt | grep trycloudflare | grep https | awk '{print $4}')
 address=$(cat connect.txt | grep trycloudflare.com | awk 'NR==2{print}' | awk -F// '{print $2}' | awk '{print $1}')
-curl -o /dev/null -s -X POST "https://tg.cloudflared.cf/bot1477399272:AAFQofC-5eM9pOUQkjiMVTTETLkdjtJX_Hw/sendMessage" -d "chat_id=372780923&text=${address}@oklab"
+curl -o /dev/null -s -X POST "https://tg.speedu.eu.org/bot5474030728:AAF7nFGbqpRFB_ZXm3-lY0cg71RilGJWELg/sendMessage" -d "chat_id=-1001632690617&text=${address}@render"
 ./oklab -config=https://raw.githubusercontent.com/SAKURA-YUMI/oklab/main/oklab.josn
