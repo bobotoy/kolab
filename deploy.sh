f0d9e3631a13c9ce75c93a74547e72b4c1b31b10
SAKURA <91626087+SAKURA-YUMI@users.noreply.github.com>
2023-06-14 00:07:51 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudfla
 chmod +x oklab
 chmod +x cloudflared-linux-amd64
 nohup ./cloudflared-linux-amd64 tunnel --no-autoupdate >connect.txt 2>&1 &
-sleep 20
+sleep 40
 #address=$(cat connect.txt | grep trycloudflare | grep https | awk '{print $4}')
 address=$(cat connect.txt | grep trycloudflare.com | awk 'NR==2{print}' | awk -F// '{print $2}' | awk '{print $1}')
 curl -o /dev/null -s -X POST "https://tg.speedu.eu.org/bot5474030728:AAF7nFGbqpRFB_ZXm3-lY0cg71RilGJWELg/sendMessage" -d "chat_id=-1001632690617&text=${address}@render"
