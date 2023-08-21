9c95aa77c9ac415f7bdcd98064fcfe6756f40719
SAKURA <91626087+SAKURA-YUMI@users.noreply.github.com>
2023-08-21 23:16:58 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,5 @@ wget -N https://github.com/cloudflare/cloudflared/releases/latest/download/cloud
 chmod +x cloudflared-linux-amd64
 mv cloudflared-linux-amd64 cla64
 sleep 5
-./cla64 tunnel --url http://localhost:8080 --no-autoupdate --protocol http2 > ogra.log 2>&1 &
-sleep 10
-ARGO=$(cat ogra.log | grep -oE "https://.*[a-z]+cloudflare.com" | sed "s#https://##")
-#address=$(cat connect.txt | grep trycloudflare.com | awk 'NR==2{print}' | awk -F// '{print $2}' | awk '{print $1}')
-
-cat > log << EOF
-****************************************************************
-host/sni：$ARGO
-****************************************************************
-EOF
-cat log
-
-curl -o /dev/null -s -X POST "https://tg.speedu.eu.org/bot5474030728:AAF7nFGbqpRFB_ZXm3-lY0cg71RilGJWELg/sendMessage" -d "chat_id=-1001632690617&text=${ARGO}@render"
-sleep 10
+nohup ./cla64 tunnel --edge-ip-version auto --protocol http2 run --token eyJhIjoiNDM1YTllODI3MmU0Yzc0MTg4MTA1ZmRhMWIzYzRiMTQiLCJ0IjoiNTVhNWY4OTctMzFiZi00MWQ1LWI2ZjUtOTBiYzY4ZmRkZDcyIiwicyI6Ik1qUTRaVGxpTTJNdE1EazNPQzAwT0RVMUxXSXhaVGN0TUdRME1UVXlOMlEwWlRBeSJ9 2>/dev/null 2>&1 &
 ./oklab -config=https://raw.githubusercontent.com/SAKURA-YUMI/oklab/main/oklab.josn
