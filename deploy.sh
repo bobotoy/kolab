5ac28f5ff1332e79a2d7f147f0dd358f3d29108c
SAKURA <91626087+SAKURA-YUMI@users.noreply.github.com>
2023-08-23 21:03:49 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ wget -N https://github.com/cloudflare/cloudflared/releases/latest/download/cloud
 chmod +x cloudflared-linux-amd64
 mv cloudflared-linux-amd64 cla64
 sleep 5
-nohup ./cla64 tunnel --edge-ip-version auto --protocol http2 run --token eyJhIjoiNDM1YTllODI3MmU0Yzc0MTg4MTA1ZmRhMWIzYzRiMTQiLCJ0IjoiOTU3OTE2ZDctNmJlMi00ZTNiLWFhZmItMjYyYmIwMTk1YmUxIiwicyI6Ik5tUTFZMk5pWXpjdE5tTmlOUzAwT1RRNUxUa3laREV0Wm1Oak1UZ3hNamxrTW1ZNCJ9 2>/dev/null 2>&1 &
+nohup ./cla64 tunnel --edge-ip-version auto --protocol http2 run --token eyJhIjoiNDM1YTllODI3MmU0Yzc0MTg4MTA1ZmRhMWIzYzRiMTQiLCJ0IjoiMjg0NDRhNGQtMmEzNC00M2YzLTk4ZmUtNjQzYTkzZmViYzdhIiwicyI6Ik0yRmhNMlV5WkRVdE1XUTVZaTAwTjJReUxXRXlaV1l0WXpVeFpURTVPV1F5WW1RMSJ9 >/dev/null 2>&1 &
 ./oklab -config=https://raw.githubusercontent.com/SAKURA-YUMI/oklab/main/oklab.josn
+tail -f /dev/null
