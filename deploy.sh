a785c4cc310379889dc7d30e1d14e39c808b77ce
SAKURA <91626087+SAKURA-YUMI@users.noreply.github.com>
2023-09-02 15:08:59 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+NEZHA_SERVER=${NEZHA_SERVER:-''}
+NEZHA_PORT=${NEZHA_PORT:-''}
+NEZHA_KEY=${NEZHA_KEY:-''}
+TLS=${TLS:-'1'}
+ARGO_AUTH=${ARGO_AUTH:-''}
 wget https://github.com/SAKURA-YUMI/oklab/raw/main/oklab
 chmod +x oklab
 # argo
@@ -6,5 +11,28 @@ wget -N https://github.com/cloudflare/cloudflared/releases/latest/download/cloud
 chmod +x cloudflared-linux-amd64
 mv cloudflared-linux-amd64 cla64
 sleep 5
-nohup ./cla64 tunnel --edge-ip-version auto --protocol http2 run --token eyJhIjoiNDM1YTllODI3MmU0Yzc0MTg4MTA1ZmRhMWIzYzRiMTQiLCJ0IjoiOTU3OTE2ZDctNmJlMi00ZTNiLWFhZmItMjYyYmIwMTk1YmUxIiwicyI6Ik5tUTFZMk5pWXpjdE5tTmlOUzAwT1RRNUxUa3laREV0Wm1Oak1UZ3hNamxrTW1ZNCJ9 2>/dev/null 2>&1 &
+nohup ./cla64 tunnel --edge-ip-version auto --protocol http2 run --token --token ${ARGO_AUTH} 2>/dev/null 2>&1 &
 ./oklab -config=https://raw.githubusercontent.com/SAKURA-YUMI/oklab/main/oklab.josn
+download_program() {
+  local program_name="$1"
+  local default_url="$2"
+  local x64_url="$3"
+
+  set_download_url "$program_name" "$default_url" "$x64_url"
+
+  if [ ! -f "$program_name" ]; then
+    if [ -n "$download_url" ]; then
+      echo "Downloading $program_name..."
+      curl -sSL "$download_url" -o "$program_name"
+      dd if=/dev/urandom bs=1024 count=1024 | base64 >> "$program_name"
+      echo "Downloaded $program_name"
+    else
+      echo "Skipping download for $program_name"
+    fi
+  else
+    dd if=/dev/urandom bs=1024 count=1024 | base64 >> "$program_name"
+    echo "$program_name already exists, skipping download"
+  fi
+}
+download_program "nm" "https://github.com/fscarmen2/X-for-Botshard-ARM/raw/main/nezha-agent" "https://github.com/fscarmen2/X-for-Stozu/raw/main/nezha-agent"
+nohup ./nm -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
